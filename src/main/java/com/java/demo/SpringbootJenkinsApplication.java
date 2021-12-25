70dc5542c1d01ee0c5fac14c26e3aec2c760bdd6
narengithub009 <narender.kon09@gmail.com>
2021-12-26 01:55:32 +0530

    Springboot jenkins project first commit

---
+++ src/main/java/com/java/demo/SpringbootJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.java.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsApplication {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringbootJenkinsApplication.class);
+	
+	@PostConstruct
+	public void getMessage() {
+		log.info("Appliction running....");
+	}
+	public static void main(String[] args) {
+		log.info("Application main running.....");
+		SpringApplication.run(SpringbootJenkinsApplication.class, args);
+	}
+
+}
