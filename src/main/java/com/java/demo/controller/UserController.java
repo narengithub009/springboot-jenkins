3fd5ed3f35c491ed568f655ecfefc56ee3760377
narengithub009 <narender.kon09@gmail.com>
2022-01-04 21:28:26 +0530

    added saveUsers and dao class

--- src/main/java/com/java/demo/controller/UserController.java
+++ src/main/java/com/java/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.java.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,14 @@ public class UserController {
 		
 		User user2=new User(1, "Naren", 34);
 		return new ResponseEntity<User>(user2, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<User> saveUsers(@RequestBody User user) {
+		
+		
+		return null;
+		
+		
+		
 	}
 }
---
+++ src/main/java/com/java/demo/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.java.demo.dao;
+
+public class UserDao {
+
+}
