8593ac96a77d2f452019f3389f2f1d6f1a9ec953
narengithub009 <narender.kon09@gmail.com>
2022-01-04 20:04:36 +0530

    added getuser method

--- src/main/java/com/java/demo/controller/UserController.java
+++ src/main/java/com/java/demo/controller/UserController.java
@@ -1,5 +1,21 @@
 package com.java.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.java.demo.model.User;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
+	@GetMapping
+	public ResponseEntity<User> getAllUsers(){
+		
+		User user2=new User(1, "Naren", 34);
+		return new ResponseEntity<User>(user2, HttpStatus.OK);
+	}
 }
--- src/main/java/com/java/demo/model/User.java
+++ src/main/java/com/java/demo/model/User.java
@@ -1,5 +1,45 @@
 package com.java.demo.model;
 
 public class User {
+	
+	private long id;
+	private String name;
+	private int age;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public User(long id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
