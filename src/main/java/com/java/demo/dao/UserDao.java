7c897088abf995687aa2a2fcd1f2686438fd051a
narengithub009 <narender.kon09@gmail.com>
2022-01-05 08:56:45 +0530

    Dao and pom.xml updates

--- src/main/java/com/java/demo/dao/UserDao.java
+++ src/main/java/com/java/demo/dao/UserDao.java
@@ -1,5 +1,9 @@
 package com.java.demo.dao;
 
-public class UserDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.java.demo.model.User;
+
+public interface UserDao extends JpaRepository<User, Long>{
 
 }
