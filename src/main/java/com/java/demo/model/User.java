a33ef0aaa2aa78e4dc57b391839fe3068f7e06f7
narengithub009 <narender.kon09@gmail.com>
2022-01-05 09:04:29 +0530

    Properties file updated

--- src/main/java/com/java/demo/model/User.java
+++ src/main/java/com/java/demo/model/User.java
@@ -1,7 +1,14 @@
 package com.java.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private long id;
 	private String name;
 	private int age;
