6830e4cae9db74e92b0daa3712ef4c9ea6f79ecc
narengithub009 <narender.kon09@gmail.com>
2022-07-30 09:36:02 +0530

    todaysssssssss

--- src/main/java/com/java/demo/SpringbootJenkinsApplication.java
+++ src/main/java/com/java/demo/SpringbootJenkinsApplication.java
@@ -18,7 +18,7 @@ public class SpringbootJenkinsApplication {
 		log.info("Appliction running....");
 	}
 	public static void main(String[] args) {
-		log.info("Springboot Application running successfully successfully.....Thank you!!");
+		log.info("Springboot Application running successfully successfully.....Thank you..Thank you!!");
 		SpringApplication.run(SpringbootJenkinsApplication.class, args);
 	}
 
